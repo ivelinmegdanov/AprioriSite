3a1eaf65e8497a7850479b11b76bbf60022ce52c
ivelinmegdanov <ivelin_megdanov@abv.bg>
2022-03-08 14:36:44 +0200

    Add project files.

---
+++ AprioriSite/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using AprioriSite.Core.Constants;
+using AprioriSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AprioriSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData[MessageConstant.ErrorMessage] = "Нещо се счупи!";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
