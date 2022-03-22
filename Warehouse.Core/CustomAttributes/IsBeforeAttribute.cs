5dc2d317ed3cddd009b7c559d75f064898c360ec
ivelinmegdanov <ivelin_megdanov@abv.bg>
2022-03-22 14:54:37 +0200

    Added ValidationAttributes

---
+++ Warehouse.Core/CustomAttributes/IsBeforeAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AprioriSite.Core.CustomAttributes
+{
+    public class IsBeforeAttribute : ValidationAttribute
+    {
+        private readonly string propertyToCompare;
+
+        public IsBeforeAttribute(string _propertyToCompare, string ErrorMessage = "")
+        {
+            propertyToCompare = _propertyToCompare;
+            this.ErrorMessage = ErrorMessage;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            try
+            {
+                DateTime dateToCompare = (DateTime)validationContext.ObjectType.GetProperty(propertyToCompare).GetValue(validationContext.ObjectInstance);
+
+                if ((DateTime)value < dateToCompare)
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return new ValidationResult(ErrorMessage);
+        }
+    }
+}
